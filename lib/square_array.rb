f34932d146c49b8f3b574bffc1628e36e0969dac
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-11 21:35:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,8 @@ def square_array(array)
   new_array = []
   counter = 0
 while array[counter] do
-  new_array << array[counter]
+  new_array << array[counter]*array[counter]
   counter += 1
 end
-  
-end 
 new_array 
 end
